933eef10770c9819af717477863919191a4c079c
mountainguy76935 <stoll33david@gmail.com>
2020-04-16 11:15:13 -0600

    testing mobile

--- src/components/circles/circles.component.jsx
+++ src/components/circles/circles.component.jsx
@@ -14,6 +14,8 @@ export const Circles = (props) => {
     return(
         <div 
             className='circles'
+            onTouchStart={handleHover}
+            onTouchEnd={null}
             onMouseEnter={handleHover}
         >
             {active ? 
